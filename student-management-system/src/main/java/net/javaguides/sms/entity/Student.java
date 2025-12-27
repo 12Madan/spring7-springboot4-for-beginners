6a6856255b0634b16ed78e6497a38fe950e3fb19
Madan Reddy <116048916+12Madan@users.noreply.github.com>
2025-12-27 14:59:12 +0530

    Update Student.java

--- student-management-system/src/main/java/net/javaguides/sms/entity/Student.java
+++ student-management-system/src/main/java/net/javaguides/sms/entity/Student.java
@@ -1,24 +1,20 @@
 package net.javaguides.sms.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Entity
+@Table(name="students")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "students")
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
     private String lastName;
-    @Column(nullable = false, unique = true)
     private String email;
 }
