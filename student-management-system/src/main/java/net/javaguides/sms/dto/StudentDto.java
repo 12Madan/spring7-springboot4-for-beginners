21e4ffac8cc67cf8a470aa37cbc22882f1f85fa8
Madan Reddy <116048916+12Madan@users.noreply.github.com>
2025-12-27 15:00:14 +0530

    Update StudentDto.java

--- student-management-system/src/main/java/net/javaguides/sms/dto/StudentDto.java
+++ student-management-system/src/main/java/net/javaguides/sms/dto/StudentDto.java
@@ -1,21 +1,14 @@
 package net.javaguides.sms.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudentDto {
+
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
-
-    // getter/setter methods
 }
